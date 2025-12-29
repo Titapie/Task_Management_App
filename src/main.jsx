d18b73e82e88cade57f9f563a2ee5a8a1f5a360a
Hồ Ngọc Hoàng Anh <149872171+Titapie@users.noreply.github.com>
2025-12-29 19:44:02 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,22 @@
-// main.jsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import { AuthProvider } from './context/AuthContext';
-import { ThemeProvider } from './context/ThemeContext'; // ✅ Import
-import './index.css'
-ReactDOM.createRoot(document.getElementById('root')).render(
-        <BrowserRouter>
-            <ThemeProvider> {/* ✅ Theme ngoài cùng */}
-                <AuthProvider> {/* ✅ Auth bên trong */}
-                    <App />
-                </AuthProvider>
-            </ThemeProvider>
-        </BrowserRouter>
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import { AuthProvider } from "./context/AuthContext";
+import { ThemeProvider } from "./context/ThemeContext";
+import { ToastProvider } from "./context/ToastContext";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <ToastProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </ToastProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
