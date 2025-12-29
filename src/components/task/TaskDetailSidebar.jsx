a30385059a027a29e17f62629ec4ca53427d5d86
Hồ Ngọc Hoàng Anh <149872171+Titapie@users.noreply.github.com>
2025-12-29 18:44:15 +0700

    Update TaskDetailSidebar.jsx

--- src/components/task/TaskDetailSidebar.jsx
+++ src/components/task/TaskDetailSidebar.jsx
@@ -3,10 +3,8 @@ export default function TaskDetailSidebar() {
     <div className="space-y-6">
       {/* Assignment */}
       <div className="bg-white dark:bg-slate-900 p-5 rounded-2xl border dark:border-slate-800">
-        <h4 className="font-bold text-slate-900 dark:text-white mb-4">
-          Assigned Assignments
-        </h4>
-        <p className="text-sm font-semibold text-slate-800 dark:text-white">
+        <h4 className="font-bold text-slate-900  mb-4">Assigned Assignments</h4>
+        <p className="text-sm font-semibold text-slate-800 ">
           Creating Awesome Mobile Apps
         </p>
         <p className="text-xs text-slate-400">UI/UX Design</p>
@@ -14,9 +12,7 @@ export default function TaskDetailSidebar() {
 
       {/* Student */}
       <div className="bg-white dark:bg-slate-900 p-5 rounded-2xl border dark:border-slate-800 space-y-3">
-        <h4 className="font-bold text-slate-900 dark:text-white">
-          Detail Student
-        </h4>
+        <h4 className="font-bold text-slate-900 ">Detail Student</h4>
         <p className="text-sm text-slate-500">Student Name: Skylar Dias</p>
         <p className="text-sm text-slate-500">Student Class: MIPA 2</p>
         <p className="text-sm text-slate-500">Student Number: 10</p>
@@ -24,7 +20,7 @@ export default function TaskDetailSidebar() {
 
       {/* Upload */}
       <div className="bg-white dark:bg-slate-900 p-5 rounded-2xl border dark:border-slate-800 space-y-4">
-        <h4 className="font-bold text-slate-900 dark:text-white">File Task</h4>
+        <h4 className="font-bold text-slate-900 ">File Task</h4>
         <div className="h-32 border-2 border-dashed border-slate-300 rounded-xl flex items-center justify-center text-xs text-slate-400">
           Drag or browse from device
         </div>
